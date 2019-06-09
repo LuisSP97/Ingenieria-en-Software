4b2cf5bb2ccc48ec732aa4f3221f2d62b5737c98
Sebastian <sebastian.perezb@utem.cl>
2019-06-09 02:28:06 -0400

    creacion plantillas indice y catalogo

--- climamega/aplicacion/views.py
+++ climamega/aplicacion/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+
+from .models import Catalogo
 
 def index(request):
-    return HttpResponse("Pagina principal")
+    return render(request, 'aplicacion/index.html')
 
 # Create your views here.
+
+def catalogo(request):
+    listaProductos = Catalogo.objects.all()
+    return render(request, 'aplicacion/catalogo.html', {'listaProductos': listaProductos})
+
+def cotizacion(request, numero_cotizacion):
+    respuesta = "Cotizacion Numero: %s."
+    return HttpResponse(respuesta % numero_cotizacion)
+
+def producto(request, codigo):
+    try:
+        producto = Catalogo.objects.get(pk=codigo)
+    except Catalogo.DoesNotExist:
+        raise Http404("No existe producto")
+    return render(request, 'aplicacion/detalleProducto.html', {'producto': producto})
