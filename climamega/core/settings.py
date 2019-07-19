d7d90643c108537b427fc91a017958388c5957be
Lester Vasquez <lesterfernaando@gmail.com>
2019-07-19 00:11:20 -0400

    Css arreglado
    css listo

--- climamega/core/settings.py
+++ climamega/core/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'clima_mega',
         'USER' : 'root',
-        'PASSWORD': 'clave',
+        'PASSWORD': 'lester123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
